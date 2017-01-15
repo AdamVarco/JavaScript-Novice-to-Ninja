f4941e64543c3e121c9d3502dcd3fc07b164552a
Natalia Kędziora <n.m.kedziora@gmail.com>
2017-01-15 19:45:46 +0100

    Project Quiz Ninja: Chapter 3 (Arrays) - quiz' questions and answers stored in an array; asking questions by iterating over quiz array (for loop)

--- quiz-ninja/js/scripts.js
+++ quiz-ninja/js/scripts.js
@@ -1,7 +1,24 @@
-// Welome the user
-alert('Welcome to Quiz Ninja');
+// quiz' questions and answers
+var quiz = [
+    ['What is Superman\'s real name?', 'Clark Kent'],
+    ['What is Wonderwoman\'s real name?', 'Dianna Prince'],
+    ['What is Batman\'s real name?', 'Bruce Wayne']
+];
 
-var question = 'What is Superman\'s real name?';
-var answer = prompt(question);
+var score = 0; // init score
 
-alert('You answered ' + answer);
+for (var i = 0; i < quiz.length; i++) {
+    // get answer from user
+    var answer = prompt(quiz[i][0]);
+
+    //check if answer is correct
+    if (answer === quiz[i][1]) {
+        alert('Correct!');
+        // increase score by 1
+        score++;
+    } else {
+        alert('Wrong!');
+    }
+}
+
+alert('Game Over, you scored ' + score + ' points');
