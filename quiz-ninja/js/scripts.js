d4cbd54b977130f7a247f842bad48cbed29602c1
Natalia Kędziora <n.m.kedziora@gmail.com>
2017-01-15 20:32:05 +0100

    Project Quiz Ninja: Chapter 4 (Functions) - rewrote game play with functions (main quiz function and helper functions)

--- quiz-ninja/js/scripts.js
+++ quiz-ninja/js/scripts.js
@@ -1,24 +1,41 @@
-// quiz' questions and answers
 var quiz = [
     ['What is Superman\'s real name?', 'Clark Kent'],
     ['What is Wonderwoman\'s real name?', 'Dianna Prince'],
     ['What is Batman\'s real name?', 'Bruce Wayne']
 ];
-
 var score = 0; // init score
 
-for (var i = 0; i < quiz.length; i++) {
-    // get answer from user
-    var answer = prompt(quiz[i][0]);
+play(quiz);
 
-    //check if answer is correct
-    if (answer === quiz[i][1]) {
-        alert('Correct!');
-        // increase score by 1
-        score++;
-    } else {
-        alert('Wrong!');
+// main function declaration
+function play(quiz) {
+    // main game loop
+    for (var i = 0, question, answer; i < quiz.length; i++) {
+        question = quiz[i][0];
+        answer = ask(question);
+
+        check(answer);
+    }
+    // end of main game loop
+    gameOver();
+
+    // helper functions
+    function ask(question) {
+        return prompt(question); // quiz[i][0] is the ith questions
+    }
+
+    function check(answer) {
+        if (answer === quiz[i][1]) {
+            // increase score by 1
+            score++;
+        } else {
+            alert('Wrong!');
+        }
+    }
+
+    function gameOver() {
+        // inform the player that the game has finished
+        // and tell them how many points they have scored
+        alert('Game over, you scored ' + score + ' points');
     }
 }
-
-alert('Game Over, you scored ' + score + ' points');
