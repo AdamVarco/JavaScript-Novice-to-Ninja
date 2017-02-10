6fb47ccc1087dab02318622345c9f785ee8e6514
natalia.k <natalia.k@webinterpret.com>
2017-02-10 13:50:10 +0100

    Chapter 10 (Testing and Debugging) - refactored isPrime() and factorsOf(); added error handling

--- jasmine-number-test/spec/numberSpec.js
+++ jasmine-number-test/spec/numberSpec.js
@@ -1,6 +1,25 @@
+var int;
+var negative;
+var decimal;
+
+beforeEach(function() {
+    int = Math.floor(100 * Math.random());
+    negative = int * -1;
+    decimal = int + 0.5;
+});
+
 describe('The factorsOf() function', function() {
+
     it('should find the factors of 12', function() {
         expect(factorsOf(12)).toEqual([1, 2, 3, 4, 6, 12]);
+    });
+
+    it('should throw an exception for negative numbers', function() {
+        expect(function() {factorsOf(negative)}).toThrow();
+    });
+
+    it('should throw an exception for non-integer numbers', function() {
+        expect(function() {factorsOf(decimal)}).toThrow();
     });
 })
 
@@ -8,7 +27,16 @@ describe('This isPrime() function', function() {
     it('should say 2 is prime', function() {
         expect(isPrime(2)).toBe(true);
     });
+
     it('should say 10 is not prime', function() {
         expect(isPrime(10)).toBe(false);
+    });
+
+    it('should say a negative number is not prime', function() {
+        expect(isPrime(negative)).toBe(false);
+    });
+
+    it('should say a non-integer number is not prime', function() {
+        expect(isPrime(decimal)).toBe(false);
     });
 });
--- jasmine-number-test/src/numberCruncher.js
+++ jasmine-number-test/src/numberCruncher.js
@@ -1,6 +1,14 @@
 'use strict';
 
 function factorsOf(number) {
+    if(number < 0) {
+        throw new RangeError('Argument Error: Number must be positive');
+    }
+
+    if(Math.floor(number) !== number) {
+        throw new RangeError('Argument Error: Number must be an integer');
+    }
+
     var factors = [];
 
     for (var i = 1; i <= Math.sqrt(number); i++) {
@@ -14,7 +22,18 @@ function factorsOf(number) {
 // This will use the factorsOf() function and check to see if the number of factors in the array returned by the factorsOf()
 // function is 2.
 // This is because all prime numbers have precisely two factors.
-
 function isPrime(number) {
+    var result;
+
+    try {
+        result = factorsOf(number).length === 2;
+    }
+    catch(e) {
+        result = false;
+    }
+    finally {
+        return result;
+    }
+
     return factorsOf(number).length === 2;
 }
